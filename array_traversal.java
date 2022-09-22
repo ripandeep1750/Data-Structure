39288fe9081acb39ae22bf9e13e7f0a1b0c60350
ripandeep1750 <71696207+ripandeep1750@users.noreply.github.com>
2022-09-22 15:03:49 +0530

    Create array_traversal.java

---
+++ array_traversal.java
@@ -0,0 +1,20 @@
+// Traversal in an array
+
+import java.util.*;
+class GFG{
+public static void main(String[] args)
+{
+	// Initialise array
+	int arr[] = { 1, 2, 3, 4 };
+
+	// size of array
+	int N = arr.length;
+
+	// Traverse the element of arr[]
+	for (int i = 0; i < N; i++) {
+
+		// Print the element
+		System.out.print(arr[i] + " ");
+	}
+}
+}
